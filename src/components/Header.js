3584b2cd0916639aefeb007fa9c8175b482151e7
Ai Shi <shiai8901@gmail.com>
2017-07-21 22:18:27 -0700

    cleaned up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import styles from '../../dist/styles.css'
 import Filter from './Filter';
 
 const Header = (props) => (
 			<div className="header">
-				<div className="container">
-					<Filter specialtyList={props.specialtyList} getDoctorsBySpecialty={props.getDoctorsBySpecialty} />
+				<div className="container" >
+					<Filter 
+						specialtyList={props.specialtyList} 
+						getDoctorsBySpecialty={props.getDoctorsBySpecialty} 
+						updateArea={props.updateArea}
+						getDoctorsByArea={props.getDoctorsByArea}
+						rating={props.rating}
+						getDoctorsByRating={props.getDoctorsByRating} />
 				</div>
 			</div>
 			)
-
+						
 
 
 export default Header
