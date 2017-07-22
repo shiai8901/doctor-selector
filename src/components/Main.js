46d1415399223fdb522a73138cfafe6227630c20
Ai Shi <shiai8901@gmail.com>
2017-07-21 22:16:47 -0700

    added a condition, render different views when in detail view or list view

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import ListView from './ListView';
 import styles from '../../dist/styles.css'
+import DetailView from './DetailView'
 
 class Main extends React.Component {
 	constructor(props) {
 		super(props);
 	}
 	render() {
-
+		if (!this.props.selectedDoctor) {
+			return (
+				<div className="main">
+					<ListView 
+						list={this.props.doctorList} 
+						setSelectedDoctor={this.props.setSelectedDoctor}
+						updateSpecialty={this.props.updateSpecialty} />
+				</div>
+			)
+		}	
 		return (
 			<div className="main">
-				Main
-				<ListView list={this.props.doctorList} />
+				<DetailView 
+					selectedDoctor={this.props.selectedDoctor} />
+				<div className="main-devider">
+					<h3>Similar doctors </h3>
+				</div>
+				<ListView 
+					list={this.props.doctorList} 
+					setSelectedDoctor={this.props.setSelectedDoctor}
+					updateSpecialty={this.props.updateSpecialty} />
 			</div>
-		)
+			)
 	}
 }
 	
 
 export default Main
+
+					
+					
