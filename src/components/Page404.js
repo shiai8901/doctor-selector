10a00e8bb0dabd529eebe7e1a113888f03d193bc
Ai Shi <shiai8901@gmail.com>
2017-07-21 22:19:17 -0700

    added a Page404 component

---
+++ src/components/Page404.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../../dist/styles.css'
+
+const Page404 = () => (
+	<div>
+		Page not found
+	</div>
+)
+export default Page404
